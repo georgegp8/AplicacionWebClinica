49e1aa215d5de8b442159959cc478a6a7fe41fa5
Alexander EP <palex2143@gmail.com>
2024-11-10 08:14:20 -0500

    actualizando dependencias para el manejo de pagos vue-scss-mask

--- stup.bat
+++ stup.bat
@@ -3,4 +3,7 @@ call composer install
 call npm install
 call copy .env.example .env
 call php artisan key:generate
+call npm install @vitejs/plugin-vue --save-dev
+call npm install vue-the-mask --save
+call npm install -D sass-embedded
 

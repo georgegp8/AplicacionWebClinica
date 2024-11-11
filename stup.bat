58277818a9a7f6360cfac05e8fa279c19b205f66
George Guerra <george.guerra@tecsup.edu.pe>
2024-11-10 22:32:23 -0500

    Dependecias para instalar ui/bootstraap  y autentificacion

--- stup.bat
+++ stup.bat
@@ -1,6 +1,6 @@
 @echo on
 call composer install
-call npm install
+call npm install && npm run dev
 call copy .env.example .env
 call php artisan key:generate
-
+call php artisan ui bootstrap --auth

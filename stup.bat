97994cfb4c94f568abe128bb73eac7e73afc8268
George Guerra <george.guerra@tecsup.edu.pe>
2024-11-12 09:24:00 -0500

    Orden de dependecias en stup.bat modificada

--- stup.bat
+++ stup.bat
@@ -1,6 +1,7 @@
 @echo on
 call composer install
-call npm install && npm run dev
+call npm install
 call copy .env.example .env
 call php artisan key:generate
 call php artisan ui bootstrap --auth
+call npm run dev

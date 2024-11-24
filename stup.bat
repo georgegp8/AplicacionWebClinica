b644d537006517fb0b2491dadc4b6cad13e978c5
George Guerra <george.guerra@tecsup.edu.pe>
2024-11-24 17:48:41 -0500

    Resolviendo conflictos en stup.bat

--- stup.bat
+++ stup.bat
@@ -3,13 +3,9 @@ call composer install
 call npm install
 call copy .env.example .env
 call php artisan key:generate
-<<<<<<< HEAD
 call npm install @vitejs/plugin-vue --save-dev
 call npm install vue-the-mask --save
 call npm install -D sass-embedded
 call php artisan storage:link
-
-=======
 call php artisan ui bootstrap --auth
 call npm run dev
->>>>>>> backend
